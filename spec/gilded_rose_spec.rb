9b6017697762ee061b74f02cb5f695e4394fdb91
Andy Henson <andrew.henson@foxsoft.net>
2014-02-12 22:33:13 +0000

    Refactor specs and increase coverage

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -2,83 +2,128 @@ require './gilded_rose.rb'
 require "rspec"
 
 describe GildedRose do
+  describe "+5 Dexterity Vest" do
+    let(:gilded_rose) { GildedRose.new(Item.new("+5 Dexterity Vest", 10, 20)) }
+  end
 
-  describe "#update_quality" do
-    it "degrades quality twice as fast when the sell by date has passed" do
-      gilded_rose = GildedRose.new(Item.new("Elixir of the Mongoose", 2, 10))
+  describe "Aged Brie" do
+    let(:gilded_rose) { GildedRose.new(Item.new("Aged Brie", 2, 0)) }
+    let(:brie) { gilded_rose.items.first }
+
+    it "increases in quality as it ages" do
       2.times { gilded_rose.update_quality }
-      elixir = gilded_rose.items.first
-      expect(elixir.sell_in).to eq(0)
-      expect(elixir.quality).to eq(8)
-      gilded_rose.update_quality
-      elixir = gilded_rose.items.first
-      expect(elixir.sell_in).to eq(-1)
-      expect(elixir.quality).to eq(6)
-    end
-    it "doesn't allow the quality of an item to be negative" do
-      gilded_rose = GildedRose.new(Item.new("Elixir of the Mongoose", 2, 10))
-      12.times { gilded_rose.update_quality }
-      elixir = gilded_rose.items.first
-      expect(elixir.sell_in).to eq(-10)
-      expect(elixir.quality).to eq(0)
-    end
-    it "increases the quality of Aged Brie as it ages" do
-      gilded_rose = GildedRose.new(Item.new("Aged Brie", 2, 0))
-      2.times { gilded_rose.update_quality }
-      brie = gilded_rose.items.first
       expect(brie.sell_in).to eq(0)
       expect(brie.quality).to eq(2)
-      4.times { gilded_rose.update_quality }
-      brie = gilded_rose.items.first
-      expect(brie.sell_in).to eq(-4)
-      expect(brie.quality).to eq(10)
     end
-    it "doesn't allow the quality of an item to be greater than 50" do
-      gilded_rose = GildedRose.new(Item.new("Aged Brie", 2, 0))
+
+    it "continues to increase in quality after the sell-in date has passed" do
+      4.times { gilded_rose.update_quality }
+      expect(brie.sell_in).to eq(-2)
+      expect(brie.quality).to eq(6)
+    end
+
+    it "does not increase quality beyond 50" do
       60.times { gilded_rose.update_quality }
-      brie = gilded_rose.items.first
       expect(brie.sell_in).to eq(-58)
       expect(brie.quality).to eq(50)
     end
-    it "does not change the quality and sell_in values for Sulfuras" do
-      gilded_rose = GildedRose.new(Item.new("Sulfuras, Hand of Ragnaros", 0, 80))
-      10.times { gilded_rose.update_quality }
-      sulfuras = gilded_rose.items.first
-      expect(sulfuras.sell_in).to eq(0)
-      expect(sulfuras.quality).to eq(80)
+  end
+
+  describe "Elixir of the Mongoose" do
+    let(:gilded_rose) { GildedRose.new(Item.new("Elixir of the Mongoose", 5, 7)) }
+    let(:elixir) { gilded_rose.items.first }
+
+    it "decreases in quality by 1 after 1 day" do
+      gilded_rose.update_quality
+      expect(elixir.sell_in).to eq(4)
+      expect(elixir.quality).to eq(6)
     end
-    it "increases the quality of Backstage passes as the sell by date approaches" do
-      gilded_rose = GildedRose.new(Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20))
+
+    it "degrades quality by 2 once the sell-in date has passed" do
+      6.times { gilded_rose.update_quality }
+      expect(elixir.sell_in).to eq(-1)
+      expect(elixir.quality).to eq(0)
+    end
+
+    it "does not allow the quality to be negative" do
+      10.times { gilded_rose.update_quality }
+      expect(elixir.sell_in).to eq(-5)
+      expect(elixir.quality).to eq(0)
+    end
+  end
+
+  describe "Sulfuras, Hand of Ragnaros" do
+    let(:gilded_rose) { GildedRose.new(Item.new("Sulfuras, Hand of Ragnaros", 0, 80)) }
+    let(:sulfuras) { gilded_rose.items.first }
+
+    it "never changes in quality" do
+      expect { gilded_rose.update_quality }.not_to change{ sulfuras.quality }
+    end
+
+    it "never expires" do
+      expect { gilded_rose.update_quality }.not_to change{ sulfuras.sell_in }
+    end
+  end
+
+  describe "Backstage passes to a TAFKAL80ETC concert" do
+    let(:gilded_rose) { GildedRose.new(Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)) }
+    let(:pass) { gilded_rose.items.first }
+
+    it "increases in quality as the sell-in date approaches" do
       5.times { gilded_rose.update_quality }
-      pass = gilded_rose.items.first
       expect(pass.sell_in).to eq(10)
       expect(pass.quality).to eq(25)
+    end
 
-      5.times { gilded_rose.update_quality }
-      pass = gilded_rose.items.first
+    it "increases in quality by 2 per day when there are 10 days or less" do
+      10.times { gilded_rose.update_quality }
       expect(pass.sell_in).to eq(5)
       expect(pass.quality).to eq(35)
+    end
 
-      5.times { gilded_rose.update_quality }
-      pass = gilded_rose.items.first
+    it "increases in quality by 3 per day when there are 5 days or less" do
+      15.times { gilded_rose.update_quality }
       expect(pass.sell_in).to eq(0)
       expect(pass.quality).to eq(50)
+    end
 
-      5.times { gilded_rose.update_quality }
-      pass = gilded_rose.items.first
+    it "is worthless after the concert" do
+      20.times { gilded_rose.update_quality }
       expect(pass.sell_in).to eq(-5)
       expect(pass.quality).to eq(0)
     end
 
-    it "degrades conjured items twice as fast as normal items" do
-      gilded_rose = GildedRose.new([Item.new("Conjured Mana Cake", 10, 20), Item.new("Mana Cake", 10, 20)])
-      5.times { gilded_rose.update_quality }
-      conjured_mana = gilded_rose.items.first
-      standard_mana = gilded_rose.items.last
-      expect(conjured_mana.sell_in).to eq(5)
-      expect(standard_mana.sell_in).to eq(5)
-      expect(conjured_mana.quality).to eq(10)
-      expect(standard_mana.quality).to eq(15)
+    it "does not have a quality greater than 50" do
+      pending("confirmation that this spec should be failing")
+      gilded_rose = GildedRose.new(Item.new("Backstage passes to a TAFKAL80ETC concert", 50, 20))
+      35.times { gilded_rose.update_quality }
+      expect(pass.sell_in).to eq(15)
+      expect(pass.quality).to eq(50)
+    end
+  end
+
+  describe "Conjured Mana Cake" do
+    let(:gilded_rose) do
+      GildedRose.new [
+        Item.new("Conjured Mana Cake", 3, 6),
+        Item.new("Mana Cake", 3, 6),
+      ]
+    end
+    let(:conjured_mana) { gilded_rose.items.first }
+    let(:mana) { gilded_rose.items.last }
+
+    it "degrades in quality twice as fast as normal items" do
+      2.times { gilded_rose.update_quality }
+
+      expect(mana.quality).to eq(4)
+      expect(conjured_mana.quality).to eq(2)
+    end
+
+    it "does not degrade quality below zero" do
+      6.times { gilded_rose.update_quality }
+
+      expect(mana.quality).to eq(0)
+      expect(conjured_mana.quality).to eq(0)
     end
   end
 end
